d3894f911fcfa6a061617e02e43cc3e0e4e1fd26
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2023-04-05 17:21:02 -0700

    Update givvyShorts.sh

--- givvyShorts.sh
+++ givvyShorts.sh
@@ -99,7 +99,7 @@ tap resource-id=\"com.givvy.shorts:id\\\/nextButton\"
 array=(\$(wm size | /data/data/com.termux/files/usr/bin/gawk {sub\(/x/\,\"\ \"\,\\\$NF\)\;print\\\$NF}))
 for i in {0..15}
 do
-    input swipe \$((\${array[1]} / 2)) \$((\${array[0]} - 10)) \$((\${array[1]} / 2)) 0 1000
+    input swipe \$((\${array[1]} / 2)) \$((\${array[0]} - 10)) \$((\${array[1]} / 2)) 0 2000
 done
 sleep 1m
 sh /system/bin/uiautomator dump /data/local/tmp/ui.xml
